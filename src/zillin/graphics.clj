aba399eb1e3452955228245134f459226a4c6e9d
Rafal Przywarski <rafal.przywarski@gmail.com>
2017-07-30 17:36:04 +0200

    triangle rasterisation with correct fill rules

--- src/zillin/graphics.clj
+++ src/zillin/graphics.clj
@@ -2,7 +2,7 @@
 
 (defprotocol Framebuffer
     (get-component [this x y i])
-    (set-component [this x y i val]))
+    (set-component! [this x y i val]))
 
 (defn- component-index [f x y i]
     (+ i (* (+ x (* y (.width f))) (.components f))))
@@ -12,8 +12,34 @@
     Framebuffer
     (get-component [this x y i]
         (aget (.pixels this) (component-index this x y i)))
-    (set-component [this x y i val]
+    (set-component! [this x y i val]
         (aset-float (.pixels this) (component-index this x y i) val)))
 
 (defn create-framebuffer [width height components]
     (ArrayFramebuffer. width height components (float-array (* width height components))))
+
+(defn rasterize-triangle! [fb x1 y1 x2 y2 x3 y3]
+    (let [edge-w (fn [x1 y1 x2 y2]
+                    (let [dx (- x2 x1)
+                          dy (- y2 y1)]
+                         (if (or (> dy 0.0) (and (= dy 0.0) (< dx 0.0)))
+                            0.0
+                            (Math/nextDown 0.0))))
+          orient (fn [x1 y1 x2 y2 xp yp]
+                    (- (* (- x2 x1) (- yp y1)) (* (- y2 y1) (- xp x1))))
+          lx (int (Math/floor (min x1 x2 x3)))
+          ly (int (Math/floor (min y1 y2 y3)))
+          ux (int (Math/ceil (max x1 x2 x3)))
+          uy (int (Math/ceil (max y1 y2 y3)))
+          ew1 (edge-w x2 y2 x3 y3)
+          ew2 (edge-w x3 y3 x1 y1)
+          ew3 (edge-w x1 y1 x2 y2)]
+         (doseq [y (range ly uy)]
+            (doseq [x (range lx ux)]
+                (let [xc (+ 0.5 x)
+                      yc (+ 0.5 y)
+                      w1 (orient x2 y2 x3 y3 xc yc)
+                      w2 (orient x3 y3 x1 y1 xc yc)
+                      w3 (orient x1 y1 x2 y2 xc yc)]
+                    (when (and (> w1 ew1) (> w2 ew2) (> w3 ew3))
+                        (set-component! fb x y 0 1)))))))
--- test/zillin/graphics_test.clj
+++ test/zillin/graphics_test.clj
@@ -4,36 +4,106 @@
 
 (deftest framebuffer-test
     (testing "creation"
-        (let [f (create-framebuffer 16 32 3)]
-            (is (= 16 (.width f)))
-            (is (= 32 (.height f)))
-            (is (= 3 (.components f)))))
+        (let [fb (create-framebuffer 16 32 3)]
+            (is (= 16 (.width fb)))
+            (is (= 32 (.height fb)))
+            (is (= 3 (.components fb)))))
     (testing "component access"
-        (let [f (create-framebuffer 8 16 3)]
-            (is (= 0.0 (get-component f 0 0 0)))
-            (is (= 0.0 (get-component f 7 3 1)))
-            (is (= 0.0 (get-component f 7 15 1)))
-            (set-component f 0 0 0 0.75)
-            (set-component f 0 0 1 -0.75)
-            (set-component f 0 0 2 -0.25)
-            (set-component f 7 0 0 1.75)
-            (set-component f 7 0 1 -1.75)
-            (set-component f 7 0 2 -1.25)
-            (set-component f 0 15 0 2.75)
-            (set-component f 0 15 1 -2.75)
-            (set-component f 0 15 2 -2.25)
-            (set-component f 7 15 0 3.75)
-            (set-component f 7 15 1 -3.75)
-            (set-component f 7 15 2 -3.25)
-            (is (= 0.75 (get-component f 0 0 0)))
-            (is (= -0.75 (get-component f 0 0 1)))
-            (is (= -0.25 (get-component f 0 0 2)))
-            (is (= 1.75 (get-component f 7 0 0)))
-            (is (= -1.75 (get-component f 7 0 1)))
-            (is (= -1.25 (get-component f 7 0 2)))
-            (is (= 2.75 (get-component f 0 15 0)))
-            (is (= -2.75 (get-component f 0 15 1)))
-            (is (= -2.25 (get-component f 0 15 2)))
-            (is (= 3.75 (get-component f 7 15 0)))
-            (is (= -3.75 (get-component f 7 15 1)))
-            (is (= -3.25 (get-component f 7 15 2))))))
+        (let [fb (create-framebuffer 8 16 3)]
+            (is (= 0.0 (get-component fb 0 0 0)))
+            (is (= 0.0 (get-component fb 7 3 1)))
+            (is (= 0.0 (get-component fb 7 15 1)))
+            (set-component! fb 0 0 0 0.75)
+            (set-component! fb 0 0 1 -0.75)
+            (set-component! fb 0 0 2 -0.25)
+            (set-component! fb 7 0 0 1.75)
+            (set-component! fb 7 0 1 -1.75)
+            (set-component! fb 7 0 2 -1.25)
+            (set-component! fb 0 15 0 2.75)
+            (set-component! fb 0 15 1 -2.75)
+            (set-component! fb 0 15 2 -2.25)
+            (set-component! fb 7 15 0 3.75)
+            (set-component! fb 7 15 1 -3.75)
+            (set-component! fb 7 15 2 -3.25)
+            (is (= 0.75 (get-component fb 0 0 0)))
+            (is (= -0.75 (get-component fb 0 0 1)))
+            (is (= -0.25 (get-component fb 0 0 2)))
+            (is (= 1.75 (get-component fb 7 0 0)))
+            (is (= -1.75 (get-component fb 7 0 1)))
+            (is (= -1.25 (get-component fb 7 0 2)))
+            (is (= 2.75 (get-component fb 0 15 0)))
+            (is (= -2.75 (get-component fb 0 15 1)))
+            (is (= -2.25 (get-component fb 0 15 2)))
+            (is (= 3.75 (get-component fb 7 15 0)))
+            (is (= -3.75 (get-component fb 7 15 1)))
+            (is (= -3.25 (get-component fb 7 15 2))))))
+
+(defn- check-rasterize-triangle [[[x1 y1] [x2 y2] [x3 y3]] [w h] expected]
+    (let [fb (create-framebuffer w h 1)]
+        (rasterize-triangle! fb x1 y1 x2 y2 x3 y3)
+        (is (= (map double expected) (seq (.pixels fb))))))
+
+(deftest rasterize-triangle-test
+    (testing "fill rules"
+        (check-rasterize-triangle [[1 1] [5 1] [1 5]] [6 6]
+            [0 0 0 0 0 0
+             0 1 1 1 0 0
+             0 1 1 0 0 0
+             0 1 0 0 0 0
+             0 0 0 0 0 0
+             0 0 0 0 0 0])
+        (check-rasterize-triangle [[1 1] [6 2] [2 4]] [7 5]
+            [0 0 0 0 0 0 0
+             0 1 1 0 0 0 0
+             0 1 1 1 1 0 0
+             0 0 1 0 0 0 0
+             0 0 0 0 0 0 0])
+        (check-rasterize-triangle [[0.5 1.5] [1.5 0.5] [1.5 1.5]] [2 2]
+            [0 0
+             0 0])
+        (check-rasterize-triangle [[0.25 1.25] [1.25 0.25] [1.25 1.25]] [2 2]
+            [0 0
+             0 0])
+        (check-rasterize-triangle [[0.5 0.5] [2.5 0.5] [0.5 2.5]] [3 3]
+            [1 1 0
+             1 0 0
+             0 0 0])
+        (check-rasterize-triangle [[0.5 2.5] [2.5 0.5] [2.5 2.5]] [3 3]
+            [0 0 0
+             0 1 0
+             0 0 0])
+        (check-rasterize-triangle [[0.5 0.5] [2.5 0.5] [0.5 2.5]] [3 3]
+            [1 1 0
+             1 0 0
+             0 0 0])
+        (check-rasterize-triangle [[0 3] [6 1] [4 3]] [6 4]
+            [0 0 0 0 0 0
+             0 0 0 0 1 0
+             0 1 1 1 0 0
+             0 0 0 0 0 0])
+        (check-rasterize-triangle [[1 3] [3 1] [4 4]] [5 5]
+            [0 0 0 0 0
+             0 0 1 0 0
+             0 1 1 0 0
+             0 0 1 1 0
+             0 0 0 0 0])
+        (check-rasterize-triangle [[1 1] [3.5 2.5] [2 4]] [4 4]
+            [0 0 0 0
+             0 1 0 0
+             0 1 1 0
+             0 0 0 0])
+        (check-rasterize-triangle [[0.75 2.5] [2.75 0.75] [4.75 2.5]] [5 3]
+            [0 0 0 0 0
+             0 0 1 1 0
+             0 0 0 0 0])
+        (check-rasterize-triangle [[0.75 1.5] [4.75 1.5] [2.5 4.25]] [6 5]
+            [0 0 0 0 0 0
+             0 1 1 1 1 0
+             0 1 1 1 0 0
+             0 0 1 0 0 0
+             0 0 0 0 0 0])
+        (check-rasterize-triangle [[1.5 2.5] [3 1] [2.5 3.5]] [4 4]
+            [0 0 0 0
+             0 0 1 0
+             0 1 1 0
+             0 0 0 0])))
