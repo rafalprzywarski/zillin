4779a6aac143ea25485ef1e486c540654a147ca3
Rafal Przywarski <rafal.przywarski@gmail.com>
2017-08-10 18:10:39 +0200

    math formatting

--- src/zillin/math.clj
+++ src/zillin/math.clj
@@ -5,30 +5,30 @@
 (defn vec3 [x y z] (Vec3. x y z))
 
 (defn add [^Vec3 l ^Vec3 r]
-    (Vec3.
-        (+ (.x l) (.x r))
-        (+ (.y l) (.y r))
-        (+ (.z l) (.z r))))
+  (Vec3.
+   (+ (.x l) (.x r))
+   (+ (.y l) (.y r))
+   (+ (.z l) (.z r))))
 
 (defn sub [^Vec3 l ^Vec3 r]
-    (Vec3.
-        (- (.x l) (.x r))
-        (- (.y l) (.y r))
-        (- (.z l) (.z r))))
+  (Vec3.
+   (- (.x l) (.x r))
+   (- (.y l) (.y r))
+   (- (.z l) (.z r))))
 
 (defn dot [^Vec3 l ^Vec3 r]
-    (+
-        (* (.x l) (.x r))
-        (* (.y l) (.y r))
-        (* (.z l) (.z r))))
+  (+
+   (* (.x l) (.x r))
+   (* (.y l) (.y r))
+   (* (.z l) (.z r))))
 
 (defn det [a b c d] (- (* a d) (* b c)))
 
 (defn cross [^Vec3 l ^Vec3 r]
-    (let [xl (.x l) xr (.x r)
-          yl (.y l) yr (.y r)
-          zl (.z l) zr (.z r)]
-         (Vec3.
-             (det yl yr zl zr)
-             (det zl zr xl xr)
-             (det xl xr yl yr))))
+  (let [xl (.x l) xr (.x r)
+        yl (.y l) yr (.y r)
+        zl (.z l) zr (.z r)]
+    (Vec3.
+     (det yl yr zl zr)
+     (det zl zr xl xr)
+     (det xl xr yl yr))))
